27449a1f2ef33f1b99d18cee320c488ca37e43f0
jodu07 <jhonnat.dussan@gmail.com>
2022-02-20 17:10:01 -0500

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { GifsModule } from './gifs/gifs.module';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+  ],    
   imports: [
-    BrowserModule
+    BrowserModule,
+    GifsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/gifs/gifs.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BusquedaComponent } from './busqueda/busqueda.component';
+import { GifsPageComponent } from './gifs-page/gifs-page.component';
+import { GifsService } from './services/gifs.service';
+import { ResultadosComponent } from './resultados/resultados.component';
+
+
+
+@NgModule({
+  declarations: [
+
+    BusquedaComponent,
+    GifsPageComponent,
+    ResultadosComponent
+  ],
+
+  exports: [
+    GifsPageComponent
+  ],
+
+  imports: [
+    CommonModule
+  ],
+  providers: [
+    GifsService
+  ],
+})
+export class GifsModule { }
