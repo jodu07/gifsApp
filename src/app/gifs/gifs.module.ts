afb2c7fa34069134403a33d49391de3ebbdbc5b8
jodu07 <jhonnat.dussan@gmail.com>
2022-03-28 23:17:26 -0500

    sidebar

--- src/app/gifs/gifs.module.ts
+++ src/app/gifs/gifs.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BusquedaComponent } from './busqueda/busqueda.component';
 import { GifsPageComponent } from './gifs-page/gifs-page.component';
-import { GifsService } from './services/gifs.service';
 import { ResultadosComponent } from './resultados/resultados.component';
 import { FormsModule } from '@angular/forms';
 
